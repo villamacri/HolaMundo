ee46747b5fd4316bc4e84d568ad98f6f22ec4c25
Cristian Daniel Villalba Marín <villalba.macri24@triana.salesianos.edu>
2023-11-14 13:57:52 +0100

    Guaaaaau

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,8 +2,9 @@ public class HolaMundo{
 
     public static void main(String[] args){
 
-    
+        System.out.println("***************");
         System.out.println("Hola Mundo");
+        System.out.println("***************");
 
     }
 
